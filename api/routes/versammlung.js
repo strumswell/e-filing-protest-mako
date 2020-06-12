53f93b422c2290d0c3028b630cd1629017854413
strumswell <philipp.bukkit@gmail.com>
2020-06-12 12:19:59 +0200

    added intern frontend

--- api/routes/versammlung.js
+++ api/routes/versammlung.js
@@ -56,11 +56,9 @@ module.exports = (app) => {
         let data = request.body;
         // Generate UUID
         data.uuid = uuidv4();
-	console.log(data);
         // Build query and execute
         let sql = "INSERT INTO versammlungen SET ?";
         let query = pool.query(sql, data, (error, results) => {
-	    console.log(error);
             // Missing or wrong attributes used
             if (error && error.code === "ER_NO_DEFAULT_FOR_FIELD") return sendResponse(response, 400, "Fehlerhafte Anfrage. Möglicherweise fehlen Attribute, check die Docs!", null);
             // Somethings wrong interally, has "code" when DB doesn't respond. Body of node error!
@@ -75,17 +73,13 @@ module.exports = (app) => {
         let data = request.body;
         // Update DB
         let sql = "UPDATE versammlungen SET ? where uuid='" + request.params.uuid + "'";
-        let status = ["offen", "inBearbeitung", "abgestimmt", "abgesagt"];
         let query = pool.query(sql, data, (error, results) => {
             // Missing or wrong attributes used
-            if (status.indexOf(data.status) == -1) return sendResponse(response, 400, "Fehlerhafte Anfrage. Möglicherweise fehlen Attribute, check die Docs!", null);
             if (error && error.code === "ER_PARSE_ERROR") return sendResponse(response, 400, "Fehlerhafte Anfrage. Möglicherweise fehlen Attribute, check die Docs!", null);
             //Somethings wrong interally, has "code" when DB doesn't respond. Body of node error!
             if (error) return sendResponse(response, 500, "Interner Fehler", null);
             // Id is unkown and no changes were made
             if (results.affectedRows < 1) return sendResponse(response, 404, "Unbekannte UUID", null);
-            // All good
-            console
             sendResponse(response, 200, null, "Versammlung aktualisiert!");
         });
     });
